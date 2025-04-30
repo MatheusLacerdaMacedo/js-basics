8ebae64fa2b51bc9cd61668d419eb794a2ff40e4
unknown <matheuslacerda412@gmail.com>
2025-04-30 14:44:59 -0300

    Exercises

---
+++ Exercises/19/index.js
@@ -0,0 +1,11 @@
+function arrayFromRange(min, max) {
+    const array= [];
+    for (let i = min; i <= max; i++) {
+        array.push(i);
+    }
+    return array;
+}
+
+const numbers = arrayFromRange(1 ,4);
+
+console.log(numbers);
