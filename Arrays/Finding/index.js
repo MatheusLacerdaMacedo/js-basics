b3c43cd3f6523983081fc701d4de7289aaad1dce
unknown <matheuslacerda412@gmail.com>
2025-04-30 13:35:59 -0300

    combining

--- Arrays/Finding/index.js
+++ Arrays/Finding/index.js
@@ -1,5 +1,18 @@
 
 const numbers = [1,2,3,1,4]
 
-console.log(numbers.indexOf(1));
+console.log(numbers.indexOf(1, 2));
 console.log(numbers.lastIndexOf(1))
+
+console.log(numbers.indexOf(1) !== -1)
+console.log(numbers.includes(1))
+
+const courses = [
+    {id:1, name:'a'},
+    {id:2, name:'b'}
+
+]
+
+const course = courses.findIndex(function(course) {
+    return course.name === 'a';
+})
---
+++ Arrays/Removing Elements/index.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4]
+
+const last = numbers.pop()
+
+const first = numbers.shift()
+
+numbers.splice(2, 2)
+console.log(numbers)
