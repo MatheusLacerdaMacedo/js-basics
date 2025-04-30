5a2caa29f871c21269459f3bf69641b48cf5fb3c
unknown <matheuslacerda412@gmail.com>
2025-04-30 17:30:45 -0300

    Functions

---
+++ Functions/Hoisting/index.js
@@ -0,0 +1,14 @@
+// Function Declaration
+walk()
+
+function walk() {
+    console.log('walk')
+}
+
+// Anonymous Function Expression
+
+run();
+
+let run = function () {
+    console.log('run')
+};
